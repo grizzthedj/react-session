551f971de4fa50bc773e44ddd76f42fdfa250b6b
grizzthedj <chrisjsmith001@gmail.com>
2020-12-14 10:46:12 -0500

    Adding tests for persistence

--- __tests__/react-session.js
+++ __tests__/react-session.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import ReactSession from '../src/ReactSession';
 
-const username = "bob";
+const username = "Bob";
+const SESSION_OBJECT_NAME = "__react_session__";
 
 describe('ReactSession', function() {
   beforeEach(() => {
@@ -18,6 +19,29 @@ describe('ReactSession', function() {
   it('Getter should get value that was set in previous test', function() {
     const sessionUsername = ReactSession.get("username");
     expect(sessionUsername).toEqual(username);
+  });
+
+  it('Getter should return value from localStorage when using localStorage store type', function() {
+    ReactSession.setStoreType("localStorage");
+    ReactSession.set("username", username);
+    const item = localStorage.getItem(SESSION_OBJECT_NAME);
+    console.log(JSON.stringify(item));
+    expect(JSON.stringify(item)).toEqual("\"{\\\"username\\\":\\\"Bob\\\"}\"");
+  });
+
+  it('Getter should return value from sessionStorage when using sessionStorage store type', function() {
+    ReactSession.setStoreType("sessionStorage");
+    ReactSession.set("username", username);
+    const item = sessionStorage.getItem(SESSION_OBJECT_NAME);
+    console.log(JSON.stringify(item));
+    expect(JSON.stringify(item)).toEqual("\"{\\\"username\\\":\\\"Bob\\\"}\"");
+  });
+
+  it('Getter should return value from cookie when using cookie store type', function() {
+    ReactSession.setStoreType("cookie");
+    ReactSession.set("username", username);
+    const cookieParam = JSON.parse(ReactSession.getCookie(SESSION_OBJECT_NAME));
+    expect(JSON.stringify(cookieParam)).toEqual("{\"username\":\"Bob\"}");
   });
 
   it('Setting invalid store type should throw an error', function() {
